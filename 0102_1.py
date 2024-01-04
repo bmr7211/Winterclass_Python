fe6d78b4386a1c2275bfc345b3b1c10382b65055
bmr7211 <133488663+bmr7211@users.noreply.github.com>
2024-01-04 13:11:57 +0900

    Add files via upload

---
+++ 0102_1.py
@@ -0,0 +1,26 @@
+'''
+age = int(input('나이를 입력하세요: '))
+tall = float(input('키를 입력하세요: '))
+
+if ((age >= 10) and (tall >= 165)):
+    print('탈 수 있어요')
+else:
+    print('탈 수 없어요')
+
+
+grade = int(input('학점을 입력하세요: '))
+score = float(input('점수를 입력하세요: '))
+
+if ((grade >= 140) and (score >= 2.0)):
+    print('졸업')
+else:
+    print('유급')
+
+
+number = int(input('정수 입력: '))
+
+if (number % 2 == 0):
+    print('짝수')
+else:
+    print('홀수')
+'''
